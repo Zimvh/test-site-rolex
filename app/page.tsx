fd2de87846f37fee0bbdf7cd6641883168f7540c
Zimvh <siti@crane-a.co.jp>
2024-03-21 13:42:00 +0800

    page

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,11 @@ import PointPurchaseRolex from "@/components/PointPurchaseRolex";
 import HigherPrice from "@/components/HigherPrice";
 import PriceTable from "@/components/PriceTable";
 import RolexHigherPrice from "@/components/RolexHigherPrice";
-import Header from "@/components/Header";
-import Intro from "@/components/Intro";
-import Search from "@/components/Search";
-import Rolexprice from "@/components/Rolexprice";
+
 import TypeOfRolex from "@/components/TypeOfRolex";
 import Faq from "@/components/Faq";
 import CustReviews from "@/components/CustReviews";
 import RolexRanking from "@/components/RolexRanking";
-import News from "@/components/News";
 
 export default function Home() {
   return (
